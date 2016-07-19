847c44a8f484b39418591d3e76234365fc22f176
Federico Iezzi <fiezzi@redhat.com>
2016-07-19 13:59:37 +0300

    Better input model  - less duplicate data csv  - less unsed data in the heat environment  - add repliace option  - better positional argument in the input model  - add capability to perform an action only from a specific csv file

--- deploy/omu-deploy.sh
+++ deploy/omu-deploy.sh
@@ -5,25 +5,25 @@ _SCRIPT=OMU
 if [[ "$1" == "" ]]
 then
 	echo "Wrapper for ${_SCRIPT} Unit Creation"
-	echo "Usage bash $0 <RC File> <Create/Update/List/Delete> [Path to the CSV Files - default "environment/omu" and it looks for admin.csv and sz.csv] [StackName - default omu1] [Instace to start with - default 1]"
+	echo "Usage bash $0 <RC File> <Create/Update/List/Delete/Replace> [Instace to start with or replace to - default 1] [Path to the CSV Files - default "environment/omu" and it looks for admin.csv and sz.csv] [StackName - default omu]"
 	exit 1
 fi
 
 _RCFILE=$1
 _ACTION=$2
-_CSVFILEPATH=${3-environment/omu}
-_STACKNAME=${4-omu}
-_INSTACE=${5-1}
+_INSTACE=${3-1}
+_CSVFILEPATH=${4-environment/omu}
+_STACKNAME=${5-omu}
 
 if [ ! -e ${_RCFILE} ]
 then
 	echo "Wrapper for ${_SCRIPT} Unit Creation"
 	echo "RC File does not exist."
-	echo "Usage bash $0 <RC File> <Create/Update/List/Delete> <ADMIN CSV File> <SZ CSV File> [StackName - default omu1] [Instace to start with - default 1]"
+	echo "Usage bash $0 <RC File> <Create/Update/List/Delete/Replace> [Instace to start with or replace to - default 1] [Path to the CSV Files - default "environment/omu" and it looks for admin.csv and sz.csv] [StackName - default omu]"
 	exit 1
 fi
 
-if [[ "${_ACTION}" != "Create" && "${_ACTION}" != "Update" && "${_ACTION}" != "List" && "${_ACTION}" != "Delete" ]]
+if [[ "${_ACTION}" != "Create" && "${_ACTION}" != "Update" && "${_ACTION}" != "List" && "${_ACTION}" != "Delete" && "${_ACTION}" != "Replace" ]]
 then
 	echo "Wrapper for ${_SCRIPT} Unit Creation"
 	echo "Action not valid."
@@ -32,7 +32,8 @@ then
 	echo "\"Update\" - To update the stack ${_STACKNAME}"
 	echo "\"List\" - To list the resource in the stack ${_STACKNAME}"
 	echo "\"Delete\" - To delete the stack ${_STACKNAME} - WARNING cannot be reversed!"
-	echo "Usage bash $0 <RC File> <Create/Update/List/Delete> <ADMIN CSV File> <SZ CSV File> [StackName - default omu1] [Instace to start with - default 1]"
+	echo "\"Replace\" - To replace a specific stack - This will delete it and then recreate it"
+	echo "Usage bash $0 <RC File> <Create/Update/List/Delete/Replace> [Instace to start with or replace to - default 1] [Path to the CSV Files - default "environment/omu" and it looks for admin.csv and sz.csv] [StackName - default omu]"
 	exit 1
 fi
 
@@ -42,14 +43,14 @@ _SZCSVFILE=$(echo ${_CSVFILEPATH}/sz.csv)
 if [ ! -f ${_ADMINCSVFILE} ] || [ ! -r ${_ADMINCSVFILE} ] || [ ! -s ${_ADMINCSVFILE} ]
 then
 	echo "Wrapper for ${_SCRIPT} Unit Creation"
-	echo "CSV File for Admin Network with mapping Portid,MacAddress,FixedIP,Netmask,Gateway does not exist."
+	echo "CSV File for Admin Network with mapping PortID,MacAddress,FixedIP does not exist."
 	exit 1
 fi
 
 if [ ! -f ${_SZCSVFILE} ] || [ ! -r ${_SZCSVFILE} ] || [ ! -s ${_SZCSVFILE} ]
 then
 	echo "Wrapper for ${_SCRIPT} Unit Creation"
-	echo "CSV File for Secure Zone Network with mapping Portid,MacAddress,FixedIP,Netmask does not exist."
+	echo "CSV File for Secure Zone Network with mapping Portid,MacAddress,FixedIP does not exist."
 	exit 1
 fi
 
@@ -61,39 +62,64 @@ cd ${_CURRENTDIR}/$(dirname $0)
 #_TENANT_NETWORK_NAME=$(cat ../environment/common.yaml|awk '/tenant_network_name/ {print $2}')
 #_TENANT_NETWORK_ID=$(neutron net-show --field id --format value ${_TENANT_NETWORK_NAME})
 
-IFS=","
-exec 3<../${_ADMINCSVFILE}
-exec 4<../${_SZCSVFILE}
-while read _ADMIN_PORTID _ADMIN_MAC _ADMIN_IP _ADMIN_NETMASK _ADMIN_GW <&3 && read _SZ_PORTID _SZ_MAC _SZ_IP _SZ_NETMASK <&4
-do
-	if [[ "${_ACTION}" != "Delete" && "${_ACTION}" != "List" ]]
-	then 
-		heat stack-$(echo "${_ACTION}" | awk '{print tolower($0)}') \
-		 --template-file ../templates/omu.yaml \
-		 --environment-file ../environment/common.yaml \
+if  [[ "${_ACTION}" != "Replace" ]]
+then
+	cat ../${_ADMINCSVFILE}|tail -n+${_INSTACE} > ../${_ADMINCSVFILE}.tmp
+	cat ../${_SZCSVFILE}|tail -n+${_INSTACE} > ../${_SZCSVFILE}.tmp
+	IFS=","
+	exec 3<../${_ADMINCSVFILE}.tmp
+	exec 4<../${_SZCSVFILE}.tmp
+	while read _ADMIN_PORTID _ADMIN_MAC _ADMIN_IP <&3 && read _SZ_PORTID _SZ_MAC _SZ_IP <&4
+	do
+		if [[ "${_ACTION}" != "Delete" && "${_ACTION}" != "List" ]]
+		then 
+			heat stack-$(echo "${_ACTION}" | awk '{print tolower($0)}') \
+			 --template-file ../templates/omu.yaml \
+			 --environment-file ../environment/common.yaml \
+	                 --parameters "unit_name=${_STACKNAME}${_INSTACE}" \
+	                 --parameters "admin_network_port=${_ADMIN_PORTID}" \
+	                 --parameters "admin_network_mac=${_ADMIN_MAC}" \
+	                 --parameters "admin_network_ip=${_ADMIN_IP}" \
+	                 --parameters "sz_network_port=${_SZ_PORTID}" \
+	                 --parameters "sz_network_mac=${_SZ_MAC}" \
+	                 --parameters "sz_network_ip=${_SZ_IP}" \
+			 ${_STACKNAME}${_INSTACE} || (echo "Error during Stack ${_ACTION}." ; exit 1)
+	                 #--parameters "tenant_network_id=${_TENANT_NETWORK_ID}" \
+		elif [[ "${_ACTION}" != "List" ]]
+		then
+			heat stack-$(echo "${_ACTION}" | awk '{print tolower($0)}') ${_STACKNAME}${_INSTACE} || (echo "Error during Stack ${_ACTION}." ; exit 1)
+		else
+			heat resource-$(echo "${_ACTION}" | awk '{print tolower($0)}') -n 20 ${_STACKNAME}${_INSTACE} || (echo "Error during Stack ${_ACTION}." ; exit 1)
+		fi
+		_INSTACE=$(($_INSTACE+1))
+	done
+elif [[ "${_ACTION}" == "Replace" ]]
+then
+        sed -n -e ${_INSTACE}p ../${_ADMINCSVFILE} > ../${_ADMINCSVFILE}.tmp
+        sed -n -e ${_INSTACE}p ../${_SZCSVFILE} > ../${_SZCSVFILE}.tmp
+	IFS=","
+	exec 3<../${_ADMINCSVFILE}.tmp
+	exec 4<../${_SZCSVFILE}.tmp
+	while read _ADMIN_PORTID _ADMIN_MAC _ADMIN_IP <&3 && read _SZ_PORTID _SZ_MAC _SZ_IP <&4
+	do
+		heat stack-delete ${_STACKNAME}${_INSTACE} || (echo "Error during Stack ${_ACTION}." ; exit 1)
+		while :; do heat resource-list ${_STACKNAME}${_INSTACE} || break; done
+                heat stack-create \
+                 --template-file ../templates/omu.yaml \
+                 --environment-file ../environment/common.yaml \
                  --parameters "unit_name=${_STACKNAME}${_INSTACE}" \
                  --parameters "admin_network_port=${_ADMIN_PORTID}" \
+                 --parameters "admin_network_mac=${_ADMIN_MAC}" \
+                 --parameters "admin_network_ip=${_ADMIN_IP}" \
                  --parameters "sz_network_port=${_SZ_PORTID}" \
-                 --parameters "admin_mac_address=${_ADMIN_MAC}" \
-                 --parameters "admin_ip_address=${_ADMIN_IP}" \
-                 --parameters "admin_netmask=${_ADMIN_NETMASK}" \
-                 --parameters "admin_gw=${_ADMIN_GW}" \
-                 --parameters "sz_mac_address=${_SZ_MAC}" \
-                 --parameters "sz_ip_address=${_SZ_IP}" \
-                 --parameters "sz_netmask=${_SZ_NETMASK}" \
-		 ${_STACKNAME}${_INSTACE} || (echo "Error during Stack ${_ACTION}." ; exit 1)
-                 #--parameters "tenant_network_id=${_TENANT_NETWORK_ID}" \
-	elif [[ "${_ACTION}" != "List" ]]
-	then
-		heat stack-$(echo "${_ACTION}" | awk '{print tolower($0)}') ${_STACKNAME}${_INSTACE} || (echo "Error during Stack ${_ACTION}." ; exit 1)
-	else
-		heat resource-$(echo "${_ACTION}" | awk '{print tolower($0)}') -n 20 ${_STACKNAME}${_INSTACE} || (echo "Error during Stack ${_ACTION}." ; exit 1)
-	fi
-	_INSTACE=$(($_INSTACE+1))
-done
+                 --parameters "sz_network_mac=${_SZ_MAC}" \
+                 --parameters "sz_network_ip=${_SZ_IP}" \
+                 ${_STACKNAME}${_INSTACE} || (echo "Error during Stack ${_ACTION}." ; exit 1)
+	done
+fi
 exec 3<&-
 exec 4<&-
-
+rm -f ../${_ADMINCSVFILE}.tmp ../${_SZCSVFILE}.tmp
 cd ${_CURRENTDIR}
 
 exit 0
