d65cb1e05cca1f80a3cf0a159ecb17478cc342d2
Federico Iezzi <fiezzi@redhat.com>
2016-07-20 19:12:30 +0300

    VLAN Guest implementation V2

--- templates/script/omu_software_config.sh
+++ templates/script/omu_software_config.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 function netconfig {
 	_SYSTEMDINIT=${1}
-	_MAC=${2}
-	_IP=${3}
-	_NETMASK=${4}
+	_VLAN=${2}
+	_MAC=${3}
+	_IP=${4}
+	_NETMASK=${5}
 	_ETH=$(ip -oneline link|awk '/'${_MAC}'/ {print $2}'|sed "s/://g")
 	_NETFILE="/etc/sysconfig/network-scripts/ifcfg-${_ETH}"
-	rm -f  ${_NETFILE} || true
 	touch ${_NETFILE}
-	echo "BOOTPROTO=static" >> ${_NETFILE}
-	echo "DEVICE=${_ETH}" >> ${_NETFILE}
-	echo "ONBOOT=yes" >> ${_NETFILE}
-	echo "IPADDR=${_IP}" >> ${_NETFILE}
-	echo "NETMASK=${_NETMASK}" >> ${_NETFILE}
-	if [[ "${5}" != "" ]]
+	if [[ "${_VLAN}" == "none" ]]
 	then
-		_GW=${5}
-		echo "GATEWAY=${_GW}" >> ${_NETFILE}
-	fi
-	echo "TYPE=Ethernet" >> ${_NETFILE}
-	if ${_SYSTEMDINIT}
-	then
-		ifdown ${_ETH} || true
-		ifup ${_ETH} || (echo "could not configure the device ${_ETH}." ; exit 1)
+		echo "BOOTPROTO=static" >> ${_NETFILE}
+		echo "DEVICE=${_ETH}" >> ${_NETFILE}
+		echo "ONBOOT=yes" >> ${_NETFILE}
+		echo "IPADDR=${_IP}" >> ${_NETFILE}
+		echo "NETMASK=${_NETMASK}" >> ${_NETFILE}
+		if [[ "${6}" != "" ]]
+		then
+			_GW=${6}
+			echo "GATEWAY=${_GW}" >> ${_NETFILE}
+		fi
+		echo "TYPE=Ethernet" >> ${_NETFILE}
+		if ${_SYSTEMDINIT}
+		then
+			ifdown ${_ETH} || true
+			ifup ${_ETH} || (echo "could not configure the device ${_ETH}." ; exit 1)
+		fi
+	else
+                echo "DEVICE=${_ETH}" >> ${_NETFILE}
+                echo "TYPE=Ethernet" >> ${_NETFILE}
+                echo "BOOTPROTO=static" >> ${_NETFILE}
+                echo "ONBOOT=yes" >> ${_NETFILE}
+
+		touch ${_NETFILE}.${_VLAN}
+                echo "DEVICE=${_ETH}.${_VLAN}" >> ${_NETFILE}.${_VLAN}
+                echo "BOOTPROTO=none" >> ${_NETFILE}.${_VLAN}
+                echo "ONBOOT=yes" >> ${_NETFILE}.${_VLAN}
+                echo "IPADDR=${_IP}" >> ${_NETFILE}.${_VLAN}
+                echo "NETMASK=${_NETMASK}" >> ${_NETFILE}.${_VLAN}
+                echo "USERCTL=no" >> ${_NETFILE}.${_VLAN}
+                echo "VLAN=yes" >> ${_NETFILE}.${_VLAN}
+                if [[ "${6}" != "" ]]
+                then
+                        _GW=${6}
+                        echo "GATEWAY=${_GW}" >> ${_NETFILE}.${_VLAN}
+                fi
+                if ${_SYSTEMDINIT}
+                then
+                        systemctl restart network || (echo "could not configure the device ${_ETH}." ; exit 1)
+                fi
 	fi
 }
 
@@ -39,8 +65,12 @@ fi
 _SYSVINIT=$(pidof /sbin/init >/dev/null 2>&1 && echo true || echo false)
 _SYSTEMD=$(pidof systemd >/dev/null 2>&1 && echo true || echo false)
 
-netconfig ${_SYSTEMD} _ADMIN_MAC_ _ADMIN_IP_ _ADMIN_NETMASK_ _ADMIN_GW_
-netconfig ${_SYSTEMD} _SZ_MAC_ _SZ_IP_ _SZ_NETMASK_
+for _OLDNET in $(ls /etc/sysconfig/network-scripts/ifcfg-*|grep -v ifcfg-lo)
+do
+	rm -f ${_OLDNET}
+done
+netconfig ${_SYSTEMD} _ADMIN_VLAN_ _ADMIN_MAC_ _ADMIN_IP_ _ADMIN_NETMASK_ _ADMIN_GW_
+netconfig ${_SYSTEMD} _SZ_VLAN_ _SZ_MAC_ _SZ_IP_ _SZ_NETMASK_
 
 # RHEL 6.7 and 6.8 has the following bug - https://bugs.launchpad.net/cloud-init/+bug/1246485
 cat > /etc/cloud/cloud.cfg.d/99_hostname.cfg << EOF
