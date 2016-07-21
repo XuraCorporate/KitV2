6ef79425562eb8e75cc360e075f0260ba77c5877
Federico Iezzi <fiezzi@redhat.com>
2016-07-21 14:08:57 +0200

    Log and comments for preparetion stack

--- deploy/preparetion-deploy.sh
+++ deploy/preparetion-deploy.sh
@@ -3,7 +3,7 @@
 function exit_for_error {
         _MESSAGE=$1
 	_CHANGEDIR=$2
-        echo ${_MESSAGE}
+	echo -e "${RED}${_MESSAGE}${NC}"
 	if ${_CHANGEDIR}
 	then
 		cd ${_CURRENTDIR}
@@ -13,7 +13,7 @@ function exit_for_error {
 
 if [[ "$1" == "" ]]
 then
-	echo "Usage bash $0 <OpenStack RC environment file> <Create/Update/List/Delete>"
+	echo -e "${RED}Usage bash $0 <OpenStack RC environment file> <Create/Update/List/Delete>${NC}"
 	exit 1
 fi
 
@@ -21,16 +21,23 @@ _RCFILE=$1
 _ACTION=$2
 _STACKNAME=PreparetionStack
 #_STACKNAME=${3-PreparetionStack}
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
 if [ ! -e ${_RCFILE} ]
 then
+	echo -e "${RED}"
 	echo "OpenStack RC environment file does not exist."
 	echo "Usage bash $0 <OpenStack RC environment file> <Create/Update/List/Delete>"
+	echo -e "${NC}"
 	exit 1
 fi
 
 if [[ "${_ACTION}" != "Create" && "${_ACTION}" != "Update" && "${_ACTION}" != "List" && "${_ACTION}" != "Delete" ]]
 then
+	echo -e "${RED}"
 	echo "Action not valid."
 	echo "It must be in the following format:"
 	echo "\"Create\" - To create a new ${_STACKNAME}"
@@ -38,6 +45,7 @@ then
 	echo "\"List\" - To list the resource in the stack ${_STACKNAME}"
 	echo "\"Delete\" - To delete the stack ${_STACKNAME} - WARNING cannot be reversed!"
 	echo "Usage bash $0 <OpenStack RC environment file> <Create/Update/List/Delete>"
+	echo -e "${NC}"
 	exit 1
 fi
 
@@ -45,31 +53,43 @@ for _ENV in $(env|grep ^OS|awk -F "=" '{print $1}')
 do
 	unset ${_ENV}
 done
+echo -e -n "Loading environment file ...\t\t"
 source ${_RCFILE}
+echo -e "${GREEN} [OK]${NC}"
 
+echo -e -n "Verifing Heat binary ...\t\t"
 which heat > /dev/null 2>&1 || exit_for_error "Error, Cannot find pythonheat-client." false
+echo -e "${GREEN} [OK]${NC}"
+echo -e -n "Verifing OpenStack credential ...\t\t"
 heat stack-list > /dev/null 2>&1 || exit_for_error "Error, During credential validation." false
+echo -e "${GREEN} [OK]${NC}"
 
 _CURRENTDIR=$(pwd)
 cd ${_CURRENTDIR}/$(dirname $0)
 
 if [[ "${_ACTION}" != "Delete" && "${_ACTION}" != "List" ]]
 then
+	echo -e -n "Verifing if ${_STACKNAME} is already loaded ...\t\t"
 	heat resource-list ${_STACKNAME} > /dev/null 2>&1
 	if [[ "${_ACTION}" == "Create" && "${?}" == "0" ]]
 	then
 		exit_for_error "Error, The Stack already exist." true
 	fi
+	echo -e "${GREEN} [OK]${NC}"
 	
+	echo -e "Loading Stack ${_STACKNAME} ..."
 	heat stack-$(echo "${_ACTION}" | awk '{print tolower($0)}') \
 	 --template-file ../templates/preparation.yaml \
 	 --environment-file ../environment/common.yaml \
 	${_STACKNAME} || exit_for_error "Error, During Stack ${_ACTION}." true
 elif [[ "${_ACTION}" != "List" ]]
 then
+	echo -e "Seeing Stack ${_STACKNAME} ..."
 	heat stack-$(echo "${_ACTION}" | awk '{print tolower($0)}') ${_STACKNAME} || exit_for_error "Error, During Stack ${_ACTION}." true
 else
+	echo -e "Loading Stack ${_STACKNAME} ..."
 	heat resource-$(echo "${_ACTION}" | awk '{print tolower($0)}') -n 20 ${_STACKNAME} || exit_for_error "Error, During Stack ${_ACTION}." true
+	echo -e "${GREEN} [OK]${NC}"
 fi
 
 cd ${_CURRENTDIR}
